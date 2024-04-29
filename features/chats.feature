9a2a959e0aaf97ca1d5a6522b8b9e1d027241ccd
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2024-04-29 17:43:25 +0300

    Add chats

--- features/chats.feature
+++ features/chats.feature
@@ -3,6 +3,9 @@ Feature: Home page
   As a viewer
   I want to see the home page of my app
 
+  Background:
+    Given user is logged in
+
 	Scenario: View chats page
   		Given I am on the home page
   		Then I should see "Welcome to our Live Chat!"
--- features/step_definitions/chats_steps.rb
+++ features/step_definitions/chats_steps.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+Given 'user is logged in' do
+  @user = FactoryBot.create(:user, name: 'Misha', email: 'misha@livechat.com')
+  visit new_user_session_path
+  fill_in 'Email', with: @user.email
+  fill_in 'Password', with: @user.password
+  find_button('Log in').click
+end
+
 Given "I am on the home page" do
   visit "/"
 end
